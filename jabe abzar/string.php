6827346ce8d9fdebfac6a055482d0b0bd7d2d8e7
pooyavalikhanli1377 <82793951+pooyavalikhanli1377@users.noreply.github.com>
2022-02-02 14:54:44 +0330

    jabe abzar strin funtion update

--- jabe abzar/string.php
+++ jabe abzar/string.php
@@ -1,13 +1,15 @@
 <?php
+//is_string &&strlen
 $name ="1001212121212121";
-echo is_string($name).strlen( $name)."<br>";
+echo is_string($name).strlen( $name)."<hr>";
+//nl2br 
 $test ="pooyava\nlikahanli";
-echo nl2br($test); 
+echo nl2br($test)."<hr>"; 
 $num=1000000000000000000;
-echo "<br>".number_format($num);
+echo number_format($num)."<hr>";
 $arry= "hi pooya gkjdkjslsakd jdlajdlsklsds";
-echo "<br>";
 print_r(explode(" ",$arry));
+echo "<hr>";
 //str_token 
 $string2 ="hi pooya this is cscode config for php developer ";
 $tok=strtok($string2," ");
@@ -15,3 +17,33 @@ while($tok==true){
 echo" $tok <br>";
 $tok=strtok(" ");
 }
+//str-word-count funtion 
+echo"<hr>". str_word_count($string2, )."<hr>";
+//size of \
+$arry1=['1'=>'fff',"2"=>'saffff','sarrrr','tuuuuh','as'];
+var_dump($arry1);
+echo"<hr>". sizeof($arry1)."<hr>";
+//md5 funtion 
+echo md5($test);
+//stripos&&stpos
+echo "<hr>".strpos($name1,"va");
+echo "<hr>".stripos($test,"v" );
+//impload
+echo"<hr>". implode($arry1)."<hr>";
+//substr
+echo substr($test,5)."<hr>";
+//strstr
+echo strstr("pooya valikhanli asdadasdasdasdasdasd", "li",false);
+echo "<hr>".strstr("pooya valikhanli asdadasdasdasdasdasd", "li",true);
+//str-replace 
+$name1="pooya valikhanli";
+echo "<hr>".str_replace("pooya","peyman",$name1);
+//str-repeat
+echo "<hr>".str_repeat($name1,10);
+//str_shuffle
+echo "<hr>". str_shuffle($name1);
+//str-word-count
+echo "<hr>".str_word_count($name1);
+
+
+
--- time funtion/get&&date funtion.php
+++ time funtion/get&&date funtion.php
@@ -2,8 +2,8 @@
 //time stamp my age 
 date_default_timezone_set("asia/tehran");
 $date= mktime(0,0,0,8, 25,1998);
+$r=mktime(0,0,0,1,30,2022);
 echo $date;
-$r=date('Y/M/d',$date);
-echo "<hr>$r <hr>";
-echo time();
+$age =date('Y/M/d',$date);
+echo "<hr>$age <hr>";
 ?>
